7db2c3014755fc1f88e02aff550812722b6f38aa
Hemant94 <kamath.h@northeastern.edu>
2018-09-27 01:38:16 -0400

    Completed WebApp

--- webapp/src/test/java/com/cloud/CloudApplicationTests.java
+++ webapp/src/test/java/com/cloud/CloudApplicationTests.java
@@ -1,4 +1,4 @@
-package com.cloudcomputing;
+package com.cloud;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CloudComputingWebAppApplicationTests {
+public class CloudApplicationTests {
 
 	@Test
 	public void contextLoads() {
