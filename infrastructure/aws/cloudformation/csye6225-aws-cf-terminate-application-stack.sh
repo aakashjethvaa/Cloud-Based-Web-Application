3485cf52013aafd593209764032a8250d157705a
panchalkiran <panchal.k@northeastern.edu>
2018-10-04 17:09:52 -0400

    Application Files

---
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-application-stack.sh
@@ -0,0 +1,10 @@
+# Terminate
+
+echo "Please enter the Stack Name"
+read stack_name
+echo "Starting deletion of Cloudformation Stack"
+aws cloudformation delete-stack --stack-name $stack_name
+echo $?
+echo "Stack deleted successfully"
+
+
