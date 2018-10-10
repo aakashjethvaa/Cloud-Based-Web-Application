fcf68b2900dddca63831931b61f83a7abdbdd0bf
aakash jethva <jethva.a@northeastern.edu>
2018-10-09 22:10:51 -0400

    S3 Bucket, Dynamo DB, RDS Script

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -2,11 +2,20 @@ STACK_NAME=$1
 VPC_NAME="${STACK_NAME}-csye6225-vpc"
 EC2_NAME="${STACK_NAME}-csye6225-ec2"
 
+echo "Enter S3 bucket"
+read S3Bucket_Name
+
+echo "Enter Dynamodb"
+read DynamoDBTable_Name
+
+echo "Enter MyDBname"
+read MyDB_Name
+
 export vpcId=$(aws ec2 describe-vpcs --query "Vpcs[*].[CidrBlock, VpcId]" --output text|grep 10.0.0.0/16|awk '{print $2}')
 export subnetId1=$(aws ec2 describe-subnets --query 'Subnets[*].[SubnetId, VpcId, AvailabilityZone, CidrBlock]' --output text|grep 10.0.1.0/24|grep us-east-1a|awk '{print $1}')
 export eC2SecurityGroupId=$(aws ec2 describe-security-groups --query 'SecurityGroups[*].[VpcId, GroupId]' --output text|grep ${vpcId}|awk '{print $2}')
 
-aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://csye6225-cf-application.json --parameters ParameterKey=VpcId,ParameterValue=$vpcId ParameterKey=EC2Name,ParameterValue=$EC2_Name ParameterKey=EC2SecurityGroup,ParameterValue=$eC2SecurityGroupId ParameterKey=SubnetId1,ParameterValue=$subnetId1 ParameterKey=KeyName,ParameterValue="MyKeyPair"
+aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://csye6225-cf-application.json --parameters ParameterKey=VpcId,ParameterValue=$vpcId ParameterKey=EC2Name,ParameterValue=$EC2_Name ParameterKey=EC2SecurityGroup,ParameterValue=$eC2SecurityGroupId ParameterKey=SubnetId1,ParameterValue=$subnetId1 ParameterKey=KeyName,ParameterValue="MyKeyPair" ParameterKey=S3BucketName,ParameterValue=$S3Bucket_Name ParameterKey=DynamoDBTableName,ParameterValue=$DynamoDBTable_Name ParameterKey=MyDBName,ParameterValue=$MyDB_Name
 
 export STACK_STATUS=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[][ [StackStatus ] ][]" --output text)
 
