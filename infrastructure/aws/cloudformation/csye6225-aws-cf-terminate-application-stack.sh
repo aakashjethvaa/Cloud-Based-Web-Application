7c5a46f2c0ccd1cac7efeec4e0385bed1d686e45
Komal Suthar <suthar.k@husky.neu.edu>
2018-10-25 15:15:35 -0400

    My script testing

--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-application-stack.sh
@@ -7,7 +7,7 @@ aws ec2 stop-instances --instance-ids $ec2InstanceId
 aws ec2 wait instance-stopped --instance-ids $ec2InstanceId
 echo "Instance ${ec2InstanceId} is stopped!!"
 
-aws ec2 modify-instance-attribute --no-disable-api-termination --instance-id $ec2InstanceId
+aws ec2 terminate-instances --instance-id $ec2InstanceId
 echo "Termination protection is removed from the instance!!"
 
 aws cloudformation delete-stack --stack-name $STACK_NAME
