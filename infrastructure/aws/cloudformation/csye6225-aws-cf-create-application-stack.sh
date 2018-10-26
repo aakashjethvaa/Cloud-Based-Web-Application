bde2a51e1ee8abc5abd193f87b4fc381ddb557c6
Komal Suthar <suthar.k@husky.neu.edu>
2018-10-25 23:20:01 -0400

    Hi Cloud

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -1,6 +1,6 @@
 STACK_NAME=$1
 VPC_NAME="${STACK_NAME}-csye6225-vpc"
-EC2_NAME="ec2"
+EC2_NAME="${STACK_NAME}-ec2"
 EC2VOL_SIZE="16"
 EC2VOL_TYPE="gp2"
 AMI_IMAGE="ami-9887c6e7"
@@ -11,8 +11,9 @@ DB_NAME="csye6225"
 DB_INSTANCE_CLASS="db.t2.medium"
 DB_INSTANCE_IDENTIFIER="csye6225-fall2018"
 DB_ENGINE="MySQL"
-BUCKET_NAME="suthark1.me"
-
+echo "Enter Bucket Name"
+read B_NAME
+BUCKET_NAME="code-deploy.$B_NAME.me"
 export vpcId=$(aws ec2 describe-vpcs --query "Vpcs[*].[CidrBlock, VpcId]" --output text|grep 10.0.0.0/16|awk '{print $2}')
 echo "VpcId : ${vpcId}"
 export subnetId1=$(aws ec2 describe-subnets --query 'Subnets[*].[SubnetId, VpcId, AvailabilityZone, CidrBlock]' --output text|grep 10.0.1.0/24|grep us-east-1a|awk '{print $1}')
@@ -28,7 +29,7 @@ echo "rDSSecurityGroupId : ${rDSSecurityGroupId}"
 export eC2RoleName=$(aws iam list-roles --query 'Roles[*].[RoleName]' --output text|grep EC2Service|awk '{print $1}')
 echo "eC2RoleName : ${eC2RoleName}"
 
-aws cloudformation create-stack --stack-name $STACK_NAME --capabilities "CAPABILITY_NAMED_IAM" --template-body file://csye6225-cf-application.json --parameters ParameterKey=VpcId,ParameterValue=$vpcId ParameterKey=EC2Name,ParameterValue=$EC2_Name ParameterKey=EC2SecurityGroup,ParameterValue=$eC2SecurityGroupId ParameterKey=SubnetId1,ParameterValue=$subnetId1 ParameterKey=EC2VolumeSize,ParameterValue=$EC2VOL_SIZE ParameterKey=EC2VolumeType,ParameterValue=$EC2VOL_TYPE ParameterKey=AMIImage,ParameterValue=$AMI_IMAGE ParameterKey=DynamoDBName,ParameterValue=$DYNAMO_TABLE ParameterKey=MasterUsername,ParameterValue=$MASTER_USERNAME ParameterKey=MasterUserPwd,ParameterValue=$MASTER_USERPWD ParameterKey=DBName,ParameterValue=$DB_NAME ParameterKey=DBInstanceClass,ParameterValue=$DB_INSTANCE_CLASS ParameterKey=DBInstanceIdentifier,ParameterValue=$DB_INSTANCE_IDENTIFIER ParameterKey=DBEngine,ParameterValue=$DB_ENGINE ParameterKey=SubnetId2,ParameterValue=$subnetId2 ParameterKey=SubnetId3,ParameterValue=$subnetId3 ParameterKey=RDSSecurityGroup,ParameterValue=$rDSSecurityGroupId ParameterKey=BucketName,ParameterValue=$BUCKET_NAME ParameterKey=EC2RoleName,ParameterValue=$eC2RoleName
+aws cloudformation create-stack --stack-name $STACK_NAME --capabilities "CAPABILITY_NAMED_IAM" --template-body file://csye6225-cf-application.json --parameters ParameterKey=VpcId,ParameterValue=$vpcId ParameterKey=EC2Name,ParameterValue=$EC2_NAME ParameterKey=EC2SecurityGroup,ParameterValue=$eC2SecurityGroupId ParameterKey=SubnetId1,ParameterValue=$subnetId1 ParameterKey=EC2VolumeSize,ParameterValue=$EC2VOL_SIZE ParameterKey=EC2VolumeType,ParameterValue=$EC2VOL_TYPE ParameterKey=AMIImage,ParameterValue=$AMI_IMAGE ParameterKey=DynamoDBName,ParameterValue=$DYNAMO_TABLE ParameterKey=MasterUsername,ParameterValue=$MASTER_USERNAME ParameterKey=MasterUserPwd,ParameterValue=$MASTER_USERPWD ParameterKey=DBName,ParameterValue=$DB_NAME ParameterKey=DBInstanceClass,ParameterValue=$DB_INSTANCE_CLASS ParameterKey=DBInstanceIdentifier,ParameterValue=$DB_INSTANCE_IDENTIFIER ParameterKey=DBEngine,ParameterValue=$DB_ENGINE ParameterKey=SubnetId2,ParameterValue=$subnetId2 ParameterKey=SubnetId3,ParameterValue=$subnetId3 ParameterKey=RDSSecurityGroup,ParameterValue=$rDSSecurityGroupId ParameterKey=BucketName,ParameterValue=$BUCKET_NAME ParameterKey=EC2RoleName,ParameterValue=$eC2RoleName
 
 
 export STACK_STATUS=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[][ [StackStatus ] ][]" --output text)
--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-cicd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-cicd-stack.sh
@@ -1,5 +1,17 @@
 STACK_NAME=$1
+echo "Enter Bucket Name to delete"
+read BUCKET_NAME
 
+aws s3 rb s3://$BUCKET_NAME --force
+
+if [ $? -ne "0" ]
+then 
+	echo "Deletion of $BUCKET_NAME failed"
+else
+	echo "Deletion of $BUCKET_NAME Success"
+fi
+echo "Deleting $STACK_NAME"
+  
 aws cloudformation delete-stack --stack-name $STACK_NAME
 
 aws cloudformation wait stack-delete-complete --stack-name $STACK_NAME
