650b4dcab48cb42129d39d8de5d5ac6010394196
aakash jethva <jethva.a@northeastern.edu>
2018-10-25 23:19:43 -0400

    webapp

--- webapp/src/main/java/cloud/assignment2/cloudassignment2/CloudAssignment2Application.java
+++ webapp/src/main/java/cloud/assignment2/cloudassignment2/CloudAssignment2Application.java
@@ -2,16 +2,18 @@ package cloud.assignment2.cloudassignment2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CloudAssignment2Application extends SpringBootServletInitializer {
-
-
-        @Override
+public class CloudAssignment2Application extends 
+SpringBootServletInitializer {
+	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
-		return builder.sources(CloudAssigment2Application.class);
+		return builder.sources(CloudAssignment2Application.class);
 	}
+	
+
     public static void main(String[] args) {
         SpringApplication.run(CloudAssignment2Application.class, args);
     }
