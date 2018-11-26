eff635b138cc483261d006b41f1e42880cd2c66a
panchalkiran <panchal.k@northeastern.edu>
2018-11-26 14:11:15 -0500

    Modified

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
@@ -5,11 +5,4 @@ read name
 
 echo "--------------------------Creating CloudFormation Stack----------------------------------"
 aws cloudformation create-stack --stack-name $name --template-body file://csye6225-cf-networking.json 
-<<<<<<< HEAD
 
-
-
-
-
-=======
->>>>>>> 304a9d8064835fd17a3ed3bee9e9b4ffe79d71f7
