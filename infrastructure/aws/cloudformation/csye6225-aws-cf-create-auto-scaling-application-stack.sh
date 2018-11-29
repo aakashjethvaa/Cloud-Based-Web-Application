070de3df137ff7dbbef9a70c4c596f45bbf20fc5
aakash jethva <jethva.a@northeastern.edu>
2018-11-29 01:52:17 -0500

    jmeter

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-auto-scaling-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-auto-scaling-application-stack.sh
@@ -5,14 +5,18 @@ stackname=$1
 read -p "Enter your s3 bucket domain name: " s3domain
 s3artifact="code-deploy.csye6225-fall2018-$s3domain.me.csye6225.com"
 s3attachment="csye6225-fall2018-$s3domain.me.csye6225.com"
+domain="csye6225-fall2018-$s3domain.me"
 
-hostedzone=${s3attachment::-1}
+#hostedzone=${s3attachment::-1}
 
 HZID=$(aws route53 list-hosted-zones --query 'HostedZones[0].Id' --output text)
 HZID=${HZID#*e/}
 echo $HZID
 
-export VpcId=$(aws ec2 describe-vpcs --query "Vpcs[0]. [VpcId]" --output text)
+certARN=$(aws acm list-certificates --query CertificateSummaryList[].CertificateArn  --output text)
+echo "$certARN"
+
+export VpcId=$(aws ec2 describe-vpcs --query "Vpcs[1]. [VpcId]" --output text)
 export subnetid=$(aws ec2 describe-subnets --query 'Subnets[*].[SubnetId, VpcId, AvailabilityZone, CidrBlock]' --output text|grep 10.0.1.0/24|grep us-east-1a|awk '{print $1}')
 export securitygrp=$(aws ec2 describe-security-groups --query 'SecurityGroups[0].[GroupId]' --output text| awk '{print $1}')
 echo $VpcId
@@ -23,7 +27,7 @@ echo $instanceProfileName
 echo "S3 code deploy bucket: $s3artifact"
 echo "S3 attachement bucket: $s3attachment"
 
-createOutput=$(aws cloudformation create-stack --stack-name $stackname --template-body file://csye6225-cf-auto-scaling-application.json --parameters ParameterKey=stackname,ParameterValue=$stackname ParameterKey=subnetid,ParameterValue=$subnetid ParameterKey=securitygrp,ParameterValue=$securitygrp ParameterKey=VpcId,ParameterValue=$VpcId ParameterKey=instanceProfileName,ParameterValue=$instanceProfileName ParameterKey=HostedZoneName,ParameterValue=$domain ParameterKey=s3artifact,ParameterValue=$s3artifact ParameterKey=s3attachment,ParameterValue=$s3attachment ParameterKey=hostedzone,ParameterValue=$hostedzone ParameterKey=hzid,ParameterValue=$HZID)
+createOutput=$(aws cloudformation create-stack --stack-name $stackname --template-body file://csye6225-cf-auto-scaling-application.json --parameters ParameterKey=stackname,ParameterValue=$stackname ParameterKey=subnetid,ParameterValue=$subnetid ParameterKey=securitygrp,ParameterValue=$securitygrp ParameterKey=VpcId,ParameterValue=$VpcId ParameterKey=instanceProfileName,ParameterValue=$instanceProfileName ParameterKey=s3artifact,ParameterValue=$s3artifact ParameterKey=s3attachment,ParameterValue=$s3attachment ParameterKey=hostedzone,ParameterValue=$domain ParameterKey=hzid,ParameterValue=$HZID ParameterKey=certARN,ParameterValue=$certARN)
 
 
 if [ $? -eq 0 ]; then
